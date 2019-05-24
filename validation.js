1ba4c8bee253c3bb7b784c4f544b11115a72292b
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-05-24 14:13:08 +0200

    unable send button after filling name and email

--- validation.js
+++ validation.js
@@ -26,3 +26,32 @@ function checkEmail(emial){
     let emaiReg = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
     return emaiReg.test(email);
 }
+
+function setStartCon(){
+    document.getElementById("submitBut").disabled = true;
+    document.getElementById("name").value = "Enter your name";
+    document.getElementById("email").value = "Enter your email";
+}
+
+function checkIsFilled(){
+    var email =  document.getElementById("email").value;
+    var name = document.getElementById("name").value;
+    console.log("checking");
+    console.log(email);
+    console.log(name);
+    console.log(name.length);
+
+    if(name.length > 0 && email.length >0){
+        console.log("condition true");
+        document.getElementById("submitBut").disabled = false;
+    }
+
+}
+
+function clearNameForm(){
+    document.getElementById("name").value = "";
+}
+
+function clearEmailForm(){
+    document.getElementById("email").value = "";
+}
